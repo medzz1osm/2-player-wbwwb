ce8967017802a968b8477ef2cf6566e0c575da8c
NymmkTheMink <141179443+NymmkTheMink@users.noreply.github.com>
2023-10-20 09:35:19 +0200

    new star pos

--- js/game/Camera.js
+++ js/game/Camera.js
@@ -25,7 +25,7 @@ function Camera(scene, options){
 
 	// Properties
 	self.scene = scene;
-    self.x = Game.width/4.25;
+    self.x = Game.width/4.5;
     self.y = Game.height/2;
 	self.width = Camera.WIDTH;
 	self.height = Camera.HEIGHT;
